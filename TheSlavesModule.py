b8ef22f262599caeee840078f70c4d1bf8be1c56
faidychka <100231086+SobolevVJ@users.noreply.github.com>
2024-12-04 19:54:15 +0300

    Update TheSlavesModule.py

--- TheSlavesModule.py
+++ TheSlavesModule.py
@@ -142,7 +142,7 @@ class TheSlavesrMod(loader.Module):
             "accept-encoding": "gzip, deflate, br",
             "accept-language": "ru,en;q=0.9",
             "authorization": self.config["AUTHORIZATION_HEADER"],
-            "user-agent": random_user_agent,
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 YaBrowser/24.10.0.0 Safari/537.36",
             "sec-ch-ua": '"Chromium";v="128", "Not;A=Brand";v="24", '
                         '"YaBrowser";v="24.10", "Yowser";v="2.5"',
             "sec-ch-ua-mobile": "?0",
